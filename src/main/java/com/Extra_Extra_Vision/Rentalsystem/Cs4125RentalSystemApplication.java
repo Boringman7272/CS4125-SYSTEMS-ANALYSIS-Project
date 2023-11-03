ae0f0650c6ddf759fcfeee0806b9511fcaef20f9
Carlos Cruz <Mountaindew7436@gmail.com>
2023-11-03 15:46:31 +0000

    Bones of Project made, docker not fully configured though

---
+++ src/main/java/com/Extra_Extra_Vision/Rentalsystem/Cs4125RentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Extra_Extra_Vision.Rentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4125RentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs4125RentalSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Extra_Extra_Vision/Rentalsystem/Cs4125RentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Extra_Extra_Vision.Rentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4125RentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
